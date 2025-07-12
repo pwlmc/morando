e03d7d758cea9a90c43444773a32d8e00815d782
Paweł Maciejewski <1276139+pwlmc@users.noreply.github.com>
2025-07-12 20:25:58 +0200

    feat: basic init command (#5)
    * feat: basic init command
    
    * fix: no tests
    
    ---------
    
    Co-authored-by: Paweł Maciejewski <1276139+pwlmaciejewski@users.noreply.github.com>

--- packages/cli/src/config/validateConfig.ts
+++
@@ -1,8 +0,0 @@
-import { MorandoConfig } from "./model";
-
-export default function validateConfig(
-  // oxlint-disable-next-line no-unused-vars
-  config: Record<string, unknown>
-): config is MorandoConfig {
-  return true; // Placeholder for actual validation logic e.g., using Zod
-}
