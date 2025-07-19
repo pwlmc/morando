493d243dea944a737c27923610fd2cbd94530d83
Paweł Maciejewski <1276139+pwlmaciejewski@users.noreply.github.com>
2025-07-19 17:19:32 +0200

    feat: json schema and print-config command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,15 +6,37 @@ import { CONFIG_FILE_NAME } from "./config/model";
 import { copyFile } from "fs/promises";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { readFile } from "fs/promises";
+import buildConfig from "./config/buildConfig";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const configFilePath = `${process.cwd()}/${CONFIG_FILE_NAME}`;
 
 yargs(hideBin(process.argv))
   .command("init", "Initialize a new Morando project", async () => {
-    const dest = `${process.cwd()}/${CONFIG_FILE_NAME}`;
-    const dir = dirname(fileURLToPath(import.meta.url));
-
-    await copyFile(`${dir}/../src/config/.morandorc.template.jsonc`, dest);
+    await copyFile(
+      `${__dirname}/../src/config/.morandorc.template.jsonc`,
+      configFilePath
+    );
     console.log(
-      `Initialized a new Morando project with configuration file: ${dest}`
+      `Initialized a new Morando project with configuration file: ${configFilePath}`
     );
   })
+  .command(
+    "print-config",
+    "Print the full Morando configuration for this project",
+    async () => {
+      try {
+        const configContent = await readFile(configFilePath, "utf-8");
+        console.log(buildConfig(configContent));
+      } catch {
+        console.error("Error reading configuration file", configFilePath);
+        console.log(
+          "Please ensure you have initialized the project with 'morando init'."
+        );
+        process.exit(1);
+      }
+    }
+  )
   .parse();
