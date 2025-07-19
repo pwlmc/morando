b171ac057da2385f1d4594f839de06943252d69f
Paweł Maciejewski <1276139+pwlmc@users.noreply.github.com>
2025-07-19 14:29:06 +0200

    chore: move to ESM (#8)
    * chore: change output to esm
    
    * chore: move to esm
    
    ---------
    
    Co-authored-by: Paweł Maciejewski <1276139+pwlmaciejewski@users.noreply.github.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,11 +4,15 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { CONFIG_FILE_NAME } from "./config/model";
 import { copyFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 yargs(hideBin(process.argv))
   .command("init", "Initialize a new Morando project", async () => {
     const dest = `${process.cwd()}/${CONFIG_FILE_NAME}`;
-    await copyFile(`${__dirname}/../src/config/initConfig.jsonc`, dest);
+    const dir = dirname(fileURLToPath(import.meta.url));
+
+    await copyFile(`${dir}/../src/config/.morandorc.template.jsonc`, dest);
     console.log(
       `Initialized a new Morando project with configuration file: ${dest}`
     );
